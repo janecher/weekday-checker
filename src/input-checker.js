5abfae52146a0a08965413392efa260aba1c4b26
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-24 14:54:49 -0700

    modify checkDay function don't include the answer
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- src/input-checker.js
+++ src/input-checker.js
@@ -6,18 +6,14 @@ export function isLeapYear(year) {
   }
 }
 
-export function checkDay(day, month, year, answer) {
+export function checkDay(day, month, year) {
   if (day < 1 || day > 31) {
-    answer = "Incorrect day input: input must be interger number between 1-31";
     return false;
   } else if ((month === 4 || month === 6 || month === 9 || month === 11) && day > 30) {
-    answer = "Incorrect day input: the day for April, June, September, November must be interger number between 1-30";
     return false;
   } else if (month === 2 && isLeapYear(year) && day > 29) {
-    answer ="Incorrect day input: the day for February in a leap year must be interger number between 1-29";
     return false;
   } else if (month === 2 && !isLeapYear(year) && day > 28) {
-    answer = "Incorrect day input: the day for February in a regular year must be interger number between 1-28";
     return false;
   } 
   return true;
