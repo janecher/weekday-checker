f7891afd2e5daddae705699b1d80d89996f21ca6
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-24 15:54:26 -0700

    add function to show incorrect inputs for making DRY-code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { checkDay, checkMonth, checkYear } from './input-checker.js';
+import { calculateWeekday } from './date.js';
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+function incorrectCheckInput(message) {
+  $(".incorrect-input").show();
+  $(".answer").hide();
+  $(".incorrect-input").text(message);
+}
 
 $(document).ready(function(){
   $("form#dateCalulation").submit(function(event){
@@ -7,19 +17,21 @@ $(document).ready(function(){
     const yearInput = parseInt($("#year").val());
     const monthInput = parseInt($("#month").val());
     const dayInput = parseInt($("#day").val());
-    if(!checkYear(yearInput)) {
-      $(".answer").text("Year must be a positive number");
+    if (!checkYear(yearInput)) {
+      incorrectCheckInput("Year must be a positive number");
       return;
     }
-    if(!checkMonth(monthInput)) {
-      $(".answer").text("Month must be a number between 1 and 12");
+    if (!checkMonth(monthInput)) {
+      incorrectCheckInput("Month must be a number between 1 and 12");
       return;
     }
-    if(!checkDay(dayInput, monthInput, yearInput)) {
-      $(".answer").text("Incorrect day input");
+    if (!checkDay(dayInput, monthInput, yearInput)) {
+      incorrectCheckInput("Incorrect day input (day must be a number between 1-31, or 28, 29, 30 depends on month and year)");
       return;     
     }
+    $(".incorrect-input").hide();
     let date = new Date(yearInput, monthInput-1, dayInput);   
     $(".answer").text(calculateWeekday(date));
+    $(".answer").show();
   });
 });
